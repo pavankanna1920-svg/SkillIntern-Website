b36b59b425a9a118d04f57b996c69a0c719eb977
uttaraneofficial-glitch <uttaraneofficial@gmail.com>
2025-12-27 14:01:42 +0530

    Delete gradevo-agency-V1 directory

--- gradevo-agency-V1/cypress/e2e/navigation.cy.ts
+++
@@ -1,8 +0,0 @@
-describe('Navigation', () => {
-    it('should navigate to the contact page', () => {
-        cy.visit('/');
-        cy.contains('Start a Project').click();
-        cy.url().should('include', '/contact');
-        cy.contains('Get in Touch').should('be.visible');
-    });
-});
